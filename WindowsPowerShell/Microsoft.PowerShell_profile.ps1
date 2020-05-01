27f3d61bcc9b2070e6a2409214dc13e056a05444
Brian Denicola <bjd145@yahoo.com>
2020-05-01 09:38:31 -0500

    Fixed ll and ls alias

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -3,7 +3,7 @@ $MaximumHistoryCount=4096
 
 Set-PSReadLineKeyHandler -Key UpArrow   -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
-Set-PSReadLineKeyHandler -Key Tab       -Function Complete
+Set-PSReadLineKeyHandler -Key Tab       -Function Completedules
 
 #Aliases moved to bottom after all functions are defined
 
@@ -15,6 +15,15 @@ if((Test-Path -Path (Join-Path -Path $ENV:USERPROFILE -ChildPath "Saved Games" )
 if((Test-Path -Path (Join-Path -Path $ENV:USERPROFILE -ChildPath "3D Objects" )))     { Remove-Item -Recurse -Force (Join-Path -Path $ENV:USERPROFILE -ChildPath "3D Objects" )}
 if((Test-Path -Path (Join-Path -Path $ENV:USERPROFILE -ChildPath "Favorites" )))      { Remove-Item -Recurse -Force (Join-Path -Path $ENV:USERPROFILE -ChildPath "Favorites" )}
 if((Test-Path -Path (Join-Path -Path $ENV:USERPROFILE -ChildPath "Links" )))          { Remove-Item -Recurse -Force (Join-Path -Path $ENV:USERPROFILE -ChildPath "Links" )}
+
+function New-APIMHeader {
+  param(
+    [string] $key
+  )
+  $header = @{}
+  $header.Add('Ocp-Apim-Subscription-Key', $Key)
+  return $header
+}
 
 function Get-DefaultEditor {
   $code = Get-ExecutablePath -processName "code.exe"
@@ -28,8 +37,18 @@ function Get-AllFilesAndDirectories {
   param (
     [string] $Path = $PWD.path
   )
-  Get-ChildItemColor -Path $Path -Attributes Hidden
+  (Get-ChildItem -Path $Path -Attributes Hidden) + (Get-ChildItem -Path $Path) |
+   Sort-Object -Property Type,Name |
+   Set-ChildItemColor
 }
+
+function Get-FilesAndDirectories {
+  param (
+    [string] $Path = $PWD.path
+  )
+  Get-ChildItem -Path $Path | Set-ChildItemColor
+}
+
 
 function Get-Profile
 {
@@ -57,48 +76,57 @@ function cd {
     }
 }
 
-function Get-ChildItemColor {
+function Set-ChildItemColor {
   
-  $default = $Host.UI.RawUI.ForegroundColor
-  function Write-OutputColoriezed {
-    param ( 
-      [string] $ForeGroundColor,
-      [object] $Message
-    ) 
-    $Host.UI.RawUI.ForegroundColor = $ForeGroundColor
-    Write-Output $Message
-    $Host.UI.RawUI.ForegroundColor = $default
-  }
-  
-  Invoke-Expression ("Get-ChildItem $args") | ForEach-Object {
-    if ($_.PSIsContainer -eq $true) {
-      Write-OutputColoriezed -ForeGroundColor 'Blue' -Message $_
-    }
-    elseif ($_.Extension -match '\.(zip|tar|gz|rar)$') {
-      Write-OutputColoriezed -ForeGroundColor 'DarkGray' -Message $_
-    }
-    elseif ($_.Extension -match '\.(exe|bat|cmd|py|pl|ps1|psm1|vbs|rb|reg)$') {
-      Write-OutputColoriezed -ForeGroundColor 'DarkCyan' -Message $_
-    }
-    elseif ($_.Extension -match '\.(txt|cfg|conf|ini|csv|sql|xml|config)$') {
-      Write-OutputColoriezed -ForeGroundColor 'Cyan' -Message $_
-    }
-    elseif ($_.Extension -match '\.(cs|asax|aspx.cs)$') {
-      Write-OutputColoriezed -ForeGroundColor 'Yellow' -Message $_
-    }
-    elseif ($_.Extension -match '\.(aspx|spark|master)$') {
-      Write-OutputColoriezed -ForeGroundColor 'DarkYellow' -Message $_
-    }
-    elseif ($_.Extension -match '\.(sln|csproj)$') {
-      Write-OutputColoriezed -ForeGroundColor 'Magenta' -Message $_
-    }
-    elseif ($_.Extension -match '\.(docx|doc|xls|xlsx|pdf|mobi|epub|mpp|)$') {
-      Write-OutputColoriezed -ForeGroundColor 'Gray' -Message $_
-    }
-    else {
-      Write-OutputColoriezed -ForeGroundColor $default -Message $_
+  param(
+    [Parameter(Mandatory=$True,ValueFromPipeline=$True)]
+    [object[]] $items
+  )
+
+  begin {
+    $default = $Host.UI.RawUI.ForegroundColor
+    function Write-OutputColoriezed {
+      param ( 
+        [string] $ForeGroundColor,
+        [object] $Message
+      ) 
+      $Host.UI.RawUI.ForegroundColor = $ForeGroundColor
+      Write-Output $Message
+      $Host.UI.RawUI.ForegroundColor = $default
     }
   }
+  process { 
+    foreach( $item in $items ){
+      if ($item.PSIsContainer -eq $true) {
+        Write-OutputColoriezed -ForeGroundColor 'Blue' -Message $item
+      }
+      elseif ($item.Extension -match '\.(zip|tar|gz|rar)$') {
+        Write-OutputColoriezed -ForeGroundColor 'DarkGray' -Message $item
+      }
+      elseif ($item.Extension -match '\.(exe|bat|cmd|py|pl|ps1|psm1|vbs|rb|reg)$') {
+        Write-OutputColoriezed -ForeGroundColor 'DarkCyan' -Message $item
+      }
+      elseif ($item.Extension -match '\.(txt|cfg|conf|ini|csv|sql|xml|config)$') {
+        Write-OutputColoriezed -ForeGroundColor 'Cyan' -Message $item
+      }
+      elseif ($item.Extension -match '\.(cs|asax|aspx.cs)$') {
+        Write-OutputColoriezed -ForeGroundColor 'Yellow' -Message $item
+      }
+      elseif ($item.Extension -match '\.(aspx|spark|master)$') {
+        Write-OutputColoriezed -ForeGroundColor 'DarkYellow' -Message $item
+      }
+      elseif ($item.Extension -match '\.(sln|csproj)$') {
+        Write-OutputColoriezed -ForeGroundColor 'Magenta' -Message $item
+      }
+      elseif ($item.Extension -match '\.(docx|doc|xls|xlsx|pdf|mobi|epub|mpp|)$') {
+        Write-OutputColoriezed -ForeGroundColor 'Gray' -Message $item
+      }
+      else {
+        Write-OutputColoriezed -ForeGroundColor $default -Message $item
+      }
+    }
+  }
+  end {}
 }
 
 & {
@@ -116,13 +144,14 @@ $ENV:EDITOR = Get-DefaultEditor
 
 New-Alias -Name gh    -Value Get-History 
 New-Alias -Name i     -Value Invoke-History
-New-Alias -Name ed    -Value $ENV:EDITOR
+New-Alias -Name code  -Value $ENV:EDITOR
 New-Alias -Name sudo  -Value Start-ElevatedConsole
 New-Alias -Name top   -Value (Join-PATH $ENV:SCRIPTS_HOME "TaskManager\Get-CpuLoad.ps1")
 New-Alias -Name ll    -Value Get-AllFilesAndDirectories
-New-Alias -Name ls    -Value Get-ChildItemColor
+New-Alias -Name ls    -Value Get-FilesAndDirectories
 New-Alias -Name hf    -Value Edit-HostFile
 New-Alias -Name home  -Value Set-Home
+
 
 $ChocolateyProfile = "$ENV:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
