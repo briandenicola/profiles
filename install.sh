82db9d6e14a9a4ed73eb2a3933a13993fd800309
briandenicola <brian@briandenicola.com>
2022-08-15 08:49:32 -0500

    Codespaces install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp vim/_vimrc ~/.vimrc
+cp bashrc/.bashrc ~/.
+
+#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#cp zsh/.p10k.zsh ~/.
+#cp zsh/.zshrc ~/.
+
