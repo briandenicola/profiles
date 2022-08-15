2e66a3bcebf827ed86eb24ea4c2498ff6de8c55a
briandenicola <brian@briandenicola.com>
2022-08-15 09:14:32 -0500

    Enabled zshrc

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 cp vim/_vimrc ~/.vimrc
 cp bashrc/.bashrc ~/.
 
-#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#cp zsh/.p10k.zsh ~/.
-#cp zsh/.zshrc ~/.
+cp zsh/.p10k.zsh ~/.
+cp zsh/.zshrc ~/.
 
