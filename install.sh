96fecaf123700d94a068ae77ca1325bece6337e9
briandenicola <brian@briandenicola.com>
2024-10-16 09:18:47 -0500

    🏏 Wed Oct 16 09:18:47 CDT 2024

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+cp .banner.rc ~/.
+cp .aliases.rc-simple ~/.aliases.rc
 cp bashrc/.bashrc ~/.
+
 cp vim/_vimrc-codespaces ~/.vimrc
 cp zsh/.zshrc-codespaces ~/.zshrc
-cp .aliases.rc ~/.
-cp .banner.rc ~/.
